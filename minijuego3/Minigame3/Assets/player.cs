7ffd128dd07824b41f06a883d941f92aaaed58b1
Mariacarrenonindecardona <202207813@alu.comillas.edu>
2025-01-04 23:16:22 +0100

    Ball changes color when the game is initialized

--- minijuego3/Minigame3/Assets/player.cs
+++ minijuego3/Minigame3/Assets/player.cs
@@ -7,6 +7,20 @@ public class player : MonoBehaviour
     public float jumpForce = 10f; //Height that we are going to jump
 
     public Rigidbody2D rb;
+    public SpriteRenderer sr;
+
+    public string currentColor;
+
+    public Color colorBlue;
+    public Color colorYellow;
+    public Color colorPurple;
+    public Color colorPink;
+
+
+    void Start()
+    {
+        SetRandomColor();
+    }
 
     // Update is called once per frame
     void Update()
@@ -14,6 +28,39 @@ public class player : MonoBehaviour
         if (Input.GetButtonDown("Jump") || Input.GetMouseButtonDown(0))
         {
             rb.velocity = Vector2.up * jumpForce;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        Debug.Log(collider.tag);
+    }
+
+    void SetRandomColor()
+    {
+        int index = Random.Range(0, 3);
+
+        switch(index)
+        {
+            case 0:
+                currentColor = "blue";
+                sr.color = colorBlue;
+                break;
+
+            case 1:
+                currentColor = "yellow";
+                sr.color = colorYellow;
+                break;
+
+            case 2:
+                currentColor = "purple";
+                sr.color = colorPurple;
+                break;
+
+            case 3:
+                currentColor = "pink";
+                sr.color = colorPink;
+                break;
         }
     }
 }
