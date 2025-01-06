98d3b9b003dfc1497ed04f4e9e2cf117f4de6e84
Mariacarrenonindecardona <202207813@alu.comillas.edu>
2025-01-06 19:01:46 +0100

    Mistakes solved
    Mistakes are now solved, and the game is working correctly

--- minijuego4/Minigame4/Assets/SquareFood.cs
+++ minijuego4/Minigame4/Assets/SquareFood.cs
@@ -22,4 +22,12 @@ public class SquareFood : MonoBehaviour
         this.transform.position = new Vector3(Mathf.Round(x), Mathf.Round(y), 0.0f);
     }
 
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "SquareSnake")
+        {
+            RandomizePosition();
+        }
+    }
+
 }
--- minijuego4/Minigame4/Assets/SquareSnake.cs
+++ minijuego4/Minigame4/Assets/SquareSnake.cs
@@ -6,6 +6,15 @@ public class SquareSnake : MonoBehaviour
 {
     private Vector2 _direction = Vector2.right;
 
+    private List<Transform> _segments;
+    public Transform segmentPrefab;
+
+    private void Start()
+    {
+        _segments = new List<Transform>();
+        _segments.Add(this.transform);
+    }
+
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.W))
@@ -29,6 +38,45 @@ public class SquareSnake : MonoBehaviour
 
     private void FixedUpdate()
     {
+        for (int i = _segments.Count -1; i>0; i--)
+        {
+            _segments[i].position = _segments[i - 1].position;
+        }
+
         this.transform.position = new Vector3(Mathf.Round(this.transform.position.x) + _direction.x, Mathf.Round(this.transform.position.y) + _direction.y, 0.0f);
+    }
+
+    private void Grow()
+    {
+        Transform segment = Instantiate(this.segmentPrefab);
+        segment.position = _segments[_segments.Count - 1].position;
+
+        _segments.Add(segment);
+
+    }
+
+    private void ResetState()
+    {
+        for (int i = 1; i<_segments.Count; i++)
+        {
+            Destroy(_segments[i].gameObject);
+        }
+        _segments.Clear();
+        _segments.Add(this.transform);
+
+        this.transform.position = Vector3.zero;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "SquareFood")
+        {
+            Grow();
+        }
+
+        if (other.tag == "SquareWall")
+        {
+            ResetState();
+        }
     }
 }
